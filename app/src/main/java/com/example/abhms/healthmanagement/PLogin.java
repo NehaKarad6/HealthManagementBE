de855fccdf9b1bf7f447295d2646f3a0df1261a8
NehaKarad6 <karad.neha6@gmail.com>
2017-02-13 21:42:34 +0530

    Registration form

--- app/src/main/java/com/example/abhms/healthmanagement/PLogin.java
+++ app/src/main/java/com/example/abhms/healthmanagement/PLogin.java
@@ -1,7 +1,22 @@
 package com.example.abhms.healthmanagement;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+//import android.text.TextUtils;
+//import android.view.View;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.JsonObjectRequest;
 
 public class PLogin extends AppCompatActivity {
 
@@ -9,5 +24,39 @@ public class PLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plogin);
+
+        findViewById(R.id.btnPatientLogin1).setOnClickListener(PLogin.this::login);
+    }
+
+    private void login(View view) {
+
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("userName", getUserID());
+            obj.put("password", getPassword());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Q.q(PLogin.this).add(new JsonObjectRequest("https://healthmanagement-b7215.firebaseio.com/login.json", obj, PLogin.this::success, PLogin.this::error));
+    }
+
+    private void error(VolleyError volleyError) {
+        //findViewById(R.id.progressBar).setVisibility(View.GONE);
+        //findViewById(R.id.btnRegister).setEnabled(true);
+    }
+
+    private void success(JSONObject jsonObject) {
+
+        Intent intent = new Intent(this, Patient_dash.class);
+        startActivity(intent);
+    }
+
+    private String getUserID() {
+        return ((EditText) findViewById(R.id.PatientID)).getText().toString();
+    }
+
+    private String getPassword() {
+        return ((EditText) findViewById(R.id.PatientPassword)).getText().toString();
+
     }
 }
--- app/src/main/java/com/example/abhms/healthmanagement/Register.java
+++ app/src/main/java/com/example/abhms/healthmanagement/Register.java
@@ -1,7 +1,15 @@
 package com.example.abhms.healthmanagement;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +17,66 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        findViewById(R.id.btnNext).setOnClickListener(this::click);
+
     }
+
+    private void click(View view) {
+
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("userName", getUserName());
+            obj.put("dob", getdob());
+            //obj.put("gender",getgender());
+            obj.put("address",getaddress());
+            obj.put("bloodgroup",getblood());
+            obj.put("mobileno",getmobile());
+            obj.put("email",getemail());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Q.q(Register.this).add(new JsonObjectRequest("https://healthmanagement-b7215.firebaseio.com/login.json", obj, Register.this::success, Register.this::error));
+
+    }
+
+
+    private void error(VolleyError volleyError) {
+
+    }
+
+    private void success(JSONObject jsonObject) {
+
+        Intent intent = new Intent(this, Register_emergency.class);
+        startActivity(intent);
+    }
+
+    private String getUserName() {
+        return ((EditText) findViewById(R.id.edtname)).getText().toString();
+    }
+
+    private String getdob() {
+        return ((EditText) findViewById(R.id.edtdob)).getText().toString();
+    }
+
+    private String getaddress() {
+        return ((EditText) findViewById(R.id.edtaddress)).getText().toString();
+    }
+
+
+    private String getblood() {
+        return ((EditText) findViewById(R.id.edtbg)).getText().toString();
+    }
+
+    private String getmobile() {
+        return ((EditText) findViewById(R.id.edtmobile)).getText().toString();
+    }
+
+    private String getemail() {
+        return ((EditText) findViewById(R.id.edtemail)).getText().toString();
+    }
+
+
+
 }
